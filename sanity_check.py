fb166cb7e7554ade2b9d564f0dcb2c1c1d742a1b
Kevin Zakka <kevinzakka@users.noreply.github.com>
2017-01-08 19:17:54 +0200

    Add files via upload

--- sanity_check.py
+++ sanity_check.py
@@ -8,6 +8,8 @@ from keras.preprocessing.image import load_img, img_to_array, array_to_img
 DIMS = (600, 600)
 CAT1 = 'cat1.jpg'
 CAT2 = 'cat2.jpg'
+CAT3 = 'cat3.jpg'
+CAT4 = 'cat4.jpg'
 
 def load_data(dims, img_name, view=False):
 	"""
@@ -24,12 +26,14 @@ def load_data(dims, img_name, view=False):
 	img = img / 255.0
 	return img
 
-# load 2 cat images
+# load 4 cat images
 img1 = load_data(DIMS, CAT1)
 img2 = load_data(DIMS, CAT2)
+img3 = load_data(DIMS, CAT3)
+img4 = load_data(DIMS, CAT4)
 
 # concat into tensor of shape (2, 600, 600, 3)
-input_img = np.concatenate([img1, img2], axis=0)
+input_img = np.concatenate([img1, img2, img3, img4], axis=0)
 
 # dimension sanity check
 print("Input Img Shape: {}".format(input_img.shape))
@@ -48,12 +52,11 @@ with tf.variable_scope('spatial_transformer_0'):
 	W_fc1 = tf.Variable(tf.zeros([H*W*C, n_fc]), name='W_fc1')
 
 	# identity transform
-	initial = np.array([[1., 0, 0], [0, 1., 0]])
-	initial = initial.astype('float32')
-	initial = initial.flatten()
-	print("Theta: {}".format(initial.shape))
+	theta = np.array([[1., 0, 0], [0, 1., 0]])
+	theta = theta.astype('float32')
+	theta = theta.flatten()
 
-	b_fc1 = tf.Variable(initial_value=initial, name='b_fc1')
+	b_fc1 = tf.Variable(initial_value=theta, name='b_fc1')
 	h_fc1 = tf.matmul(tf.zeros([B, H*W*C]), W_fc1) + b_fc1
 	h_trans = spatial_transformer_network(x, h_fc1)
 
@@ -62,5 +65,5 @@ sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 y = sess.run(h_trans, feed_dict={x: input_img})
 y = np.reshape(y, (B, H, W, C))
-y = array_to_img(y[0])
+y = array_to_img(y[2])
 y.show()
