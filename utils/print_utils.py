55f50da028e5c9bc80d84e7f81e0b0dad9d5ffdc
Kevin Zakka <kevinzakka@users.noreply.github.com>
2017-01-12 00:08:46 +0200

    added function for converting tensor to np array

--- utils/print_utils.py
+++ utils/print_utils.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 
+# util function for viewing value
 def view_value(x):
 	# intialize the variable
 	init_op = tf.global_variables_initializer()
@@ -8,3 +9,13 @@ def view_value(x):
 	with tf.Session() as sess:
 		sess.run(init_op)
 		print(sess.run(x))
+
+# util function for converting tensor to numpy array
+def run_op(x):
+	# intialize the variable
+	init_op = tf.global_variables_initializer()
+
+	# run the graph
+	with tf.Session() as sess:
+		sess.run(init_op)
+		return sess.run(x)
